a7ea5cdb235c2c1e5ef2639d7cc85d762d14c137
Asbjørn Thirslund <at@brackeys.com>
2014-11-18 00:27:49 +0100

    Procedural space background generation

---
+++ Assets/ProceduralBackground/NebulaGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NebulaGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Texture2D noiseTex = GenerateNebulaTexture (256, 256);
+		transform.renderer.material.mainTexture = noiseTex;
+	}
+	
+	public static Texture2D GenerateNebulaTexture (int width, int height) {
+		Texture2D noiseTex = new Texture2D (width, height);
+
+		Color[] pixels = new Color [width * height];
+
+		noiseTex.SetPixels(pixels);
+		noiseTex.Apply();
+
+		return noiseTex;
+	}
+}
---
+++ Assets/ProceduralBackground/StarfieldGenerator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarfieldGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Texture2D noiseTex = GenerateStarfield (256, 256, 170);
+		transform.renderer.material.mainTexture = noiseTex;
+	}
+	
+	public static Texture2D GenerateStarfield (int width, int height, int starCount) {
+		Texture2D noiseTex = new Texture2D (width, height);
+
+		Color[] pixels = new Color [width * height];
+
+		for (int i = starCount; i > 0; i--) {
+			int x = Random.Range (2, width-2);
+			int y = Random.Range (2, height-2);
+			float alpha = Random.Range (0.1f, 1f);
+
+			// Create center star
+			pixels [(y * width) + x] = new Color (1,1,1, alpha);
+
+			// Create sparkle around star
+			pixels [(y * width) + (x + 1)] = new Color (1,1,1, alpha/2);
+			pixels [((y + 1) * width) + x] = new Color (1,1,1, alpha/2);
+			pixels [(y * width) + (x - 1)] = new Color (1,1,1, alpha/2);
+			pixels [((y - 1) * width) + x] = new Color (1,1,1, alpha/2);
+		}
+
+		noiseTex.SetPixels(pixels);
+		noiseTex.Apply();
+
+		return noiseTex;
+	}
+}
