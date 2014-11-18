d8430436aa3dad0950340290b21ca9a0138a5bf4
Asbjørn Thirslund <at@brackeys.com>
2014-11-18 23:23:50 +0100

    Generate GUI Box Softshadow

---
+++ Assets/TestScenes/GUISoftshadow/Editor/SoftshadowEffectEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(SoftshadowEffect))]
+public class SoftshadowEffectEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        SoftshadowEffect effect = (SoftshadowEffect)target;
+        if(GUILayout.Button("Generate Softshadow"))
+        {
+            effect.GenerateBoxShadow();
+        }
+    }
+}
---
+++ Assets/TestScenes/GUISoftshadow/SoftshadowEffect.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(RectTransform))]
+public class SoftshadowEffect : MonoBehaviour {
+
+	public int size = 50;
+	public Color color;
+	public int offset = -1;
+
+	void Start () {
+		GenerateBoxShadow ();
+	}
+	
+	public void GenerateBoxShadow () {
+		RectTransform rt = GetComponent<RectTransform>();
+
+		int width = (int)rt.rect.width + size;
+		int height = (int)rt.rect.height + size;
+
+		Texture2D shadowTex = new Texture2D (width, height);
+
+		Color[] pixels = new Color [width * height];
+
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				float alpha = 1f;
+				bool insideRect = true;
+
+				if (x - offset + size/2f >= width) {
+					alpha *= 1f - ((float)((x) - width + size/2f) / (size/2f));
+					insideRect = false;
+				}
+				if (x + offset - size/2f < 0) {
+					alpha *= (float)((x) / (size/2f));
+					insideRect = false;
+				}
+				if (y - offset + size/2f >= height) {
+					alpha *= 1f - ((float)((y) - height + size/2f) / (size/2f));
+					insideRect = false;
+				}
+				if (y + offset - size/2f < 0) {
+					alpha *= (float)((y) / (size/2f));
+					insideRect = false;
+				}
+
+				if (insideRect) {
+					alpha = 0f;
+				}
+
+				pixels [(y * width) + x]  = new Color (color.r, color.g, color.b, alpha * color.a);
+			}
+		}
+
+		shadowTex.SetPixels(pixels);
+		shadowTex.Apply();
+
+		Transform child = rt.transform.FindChild (rt.name + "_shadow");
+		if (child != null) {
+			DestroyImmediate (child.gameObject);
+		}
+
+		GameObject go = new GameObject();
+		go.name = rt.name + "_shadow";
+		go.transform.parent = rt;
+		go.transform.localPosition = Vector3.zero;
+		go.transform.localRotation = Quaternion.identity;
+		Image img = go.AddComponent<Image>();
+		RectTransform imgrt = img.GetComponent<RectTransform>();
+		imgrt.sizeDelta = new Vector2 (rt.sizeDelta.x + size, rt.sizeDelta.y + size);
+
+		Sprite shadowSprite = Sprite.Create(shadowTex, new Rect (0,0, shadowTex.width, shadowTex.height), new Vector2 (0.5f, 0.5f));
+		img.sprite = shadowSprite;
+	}
+}
