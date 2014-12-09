51410884b3d40ac863a2fa9000cb398f564870d4
Asbjørn Thirslund <at@brackeys.com>
2014-12-09 10:40:04 +0100

    Add UIClock

---
+++ Assets/Scripts/UI/UIClock.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------
+// Changes the text of a UI Text component to the current system time.
+//-----------------------------------------------------------------
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Text))]
+public class UIClock : MonoBehaviour {
+
+	private Text clockText;
+	
+	void Awake () {
+		clockText = GetComponent<Text> ();
+
+		if (clockText == null) {
+			Debug.LogError ("No Text component found on " + transform.name);
+			return;
+		}
+
+		InvokeRepeating ("UpdateClock", 0f, 5f);
+	}
+
+	public void UpdateClock () {
+		System.DateTime time = System.DateTime.Now;
+		clockText.text = time.ToString ("hh:mm");
+
+		Debug.Log ("Looping coroutine");
+	}
+
+}
