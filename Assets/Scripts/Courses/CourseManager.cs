44ebe1c6e34c495536527cd3c628920ead1bc522
Asbjørn Thirslund <at@brackeys.com>
2014-12-09 11:48:32 +0100

    Add CourseManager and CourseLoader for keeping track of courses at runtime

---
+++ Assets/Scripts/Courses/CourseManager.cs
@@ -0,0 +1,53 @@
+﻿//-----------------------------------------------------------------
+// CourseManager persists between scenes and keeps track of courses
+//-----------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CourseManager : MonoBehaviour {
+
+	private static CourseManager _ins;
+	public static CourseManager ins {
+		get {
+			if (_ins == null) {
+				_ins = GameObject.FindObjectOfType <CourseManager>();
+
+				DontDestroyOnLoad (_ins.gameObject);
+			}
+		
+			return _ins;
+		}
+		set {
+			_ins = value;
+		}
+	}
+
+	public int curCourseIndex;
+	public List<Course> courseList;
+
+	void Awake () {
+		if (_ins == null) {
+			// Populate with first instance
+			_ins = this;
+			DontDestroyOnLoad (this);
+		} else {
+			// Another instance exists, destroy
+			if (this != _ins)
+				Destroy (this.gameObject);
+		}
+
+		PopulateCourseList ();
+
+		foreach (Course course in courseList) {
+			Debug.Log (course.title);
+		}
+	}
+
+	public void PopulateCourseList () {
+		Course c = CourseUtil.LoadCourse ("Custom Course");
+		if (c != null)
+			courseList.Add (c);
+	}
+}
---
+++ Assets/Scripts/Courses/CourseUtil.cs
@@ -0,0 +1,21 @@
+﻿//-----------------------------------------------------------------
+// Hosts utility methods for handling / loading course assets at runtime.
+//-----------------------------------------------------------------
+
+using UnityEngine;
+
+public class CourseUtil {
+
+	public static Course LoadCourse (string name) {
+		Course course = Resources.LoadAssetAtPath<Course>("Assets/Courses/"
+		                                                 + name + ".asset");
+
+		if (course == null) {
+			Debug.LogError ("Course with name of "+ name +
+			                " doesn't exist. Returning null.");
+			return null;
+		}
+
+		return course;
+	}
+}
