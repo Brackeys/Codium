7437e99193b1e4e70000bb1b47991f37a4e00cde
Asbjørn Thirslund <at@brackeys.com>
2016-04-12 17:02:43 +0200

    Restructuring

---
+++ MainProject/Assets/Scripts/Skills/Data/SkillData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Codium
+{
+	[CreateAssetMenu(fileName = "Skill", menuName = "Codium/Skill", order = 1)]
+	public class SkillData : ScriptableObject
+	{
+
+		public string skillName;
+		public LessonData[] lessons;
+		
+	}
+}
